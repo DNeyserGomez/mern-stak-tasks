24c52e6e9674c43195dfcb1ef185f95a94674881
Neyser Gómez <dneysergomez@gmail.com>
2018-06-12 12:49:59 -0500

    Intalación y configuración de nodemon

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+//Settigs
+
+
+//middlewares
+
+
+//Routes
+
+
+//Statics files
+
+//Starting the server
+app.listen(3000, () => {
+    console.log('Server listen on port 3000');
+});
